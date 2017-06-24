8cd775924b71fb204ab3dbeb0052fe54cad8eb85
Vinit <vinitshaw@hotmail.com>
2017-06-23 23:22:54 -0400

    new files

---
+++ Human.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Player.h"
+
+class Human : public Player
+{
+  public:
+
+  private:
+    
+}
---
+++ Player.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+#include "Hand.h"
+
+class Player
+{
+  public:
+
+  private:
+    
+}
